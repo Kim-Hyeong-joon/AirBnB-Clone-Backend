3e9abe48a3a728cc8bd30660d91c5d46bf245e9e
Hyeong-Joon <gudwns1020@naver.com>
2023-01-16 18:09:26 +0900

    #4.3 Admin

--- houses/admin.py
+++ houses/admin.py
@@ -3,4 +3,15 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = (
+        "name",
+        "price_per_night",
+        "address",
+        "pets_allowed",   
+    )
+    list_filter = (
+        "price_per_night",
+        "pets_allowed"
+    )
+    search_fields = ("address",)
--- houses/models.py
+++ houses/models.py
@@ -5,6 +5,10 @@ class House(models.Model):
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
