be681586f6bc77e2ed365271a4bf88dd8377f549
Hyeong-Joon <gudwns1020@naver.com>
2023-02-25 19:06:54 +0900

    10.4 POST Request

--- categories/urls.py
+++ categories/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.categories),
+    path("<int:pk>", views.category),
 ]
--- categories/views.py
+++ categories/views.py
@@ -4,13 +4,20 @@ from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    serializer = CategorySerializer(all_categories, many=True)
-    return Response(
-        {
-            "ok": True,
-            "categories": serializer.data,
-        },
-    )
+
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
