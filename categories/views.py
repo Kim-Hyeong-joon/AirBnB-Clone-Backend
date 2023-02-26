559cf058618a982a081e89139f68706a9e295cfe
Hyeong-Joon <gudwns1020@naver.com>
2023-02-26 10:01:37 +0900

    #10.5 is_valid()

--- categories/views.py
+++ categories/views.py
@@ -12,8 +12,11 @@ def categories(request):
         serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data)
     elif request.method == "POST":
-        print(request.data)
-        return Response({"created": True})
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"created": True})
+        else:
+            return Response(serializer.errors)
 
 
 @api_view()
