3c68cf5e42972b242c9120d75b1db401b1a2cece
Hyeong-Joon <gudwns1020@naver.com>
2023-03-06 17:04:34 +0900

    #11.16 Upload Phto

--- rooms/views.py
+++ rooms/views.py
@@ -17,6 +17,7 @@ from .serializers import (
     RoomDetailSerializer,
 )
 from reviews.serializers import ReviewSerializer
+from medias.serializers import PhotoSerializer
 
 
 class Amenities(APIView):
@@ -229,5 +230,22 @@ class RoomAmenities(APIView):
 
 
 class RoomPhotos(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+
+        except Room.DoesNotExist:
+            raise NotFound
+
     def post(self, request, pk):
-        pass
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            return Response(PhotoSerializer(photo).data)
+        else:
+            return Response(serializer.errors)
