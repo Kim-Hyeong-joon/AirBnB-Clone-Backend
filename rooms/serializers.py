9e2545c1a7e59253f774ce0a898b70de459fd5d9
Hyeong-Joon <gudwns1020@naver.com>
2023-03-07 19:38:09 +0900

    #11.21 is_liked

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -4,6 +4,7 @@ from users.serializers import TinyUserSerializer
 from reviews.serializers import ReviewSerializer
 from categories.serializers import CategorySerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 
 
 class AmenitySerializer(serializers.ModelSerializer):
@@ -20,21 +21,23 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     owner = TinyUserSerializer(
         read_only=True,
     )
+    """ 
     amenities = AmenitySerializer(
         read_only=True,
-        many=True,
-    )
+        many=True,)
+    """
     category = CategorySerializer(
         read_only=True,
     )
 
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
     photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
         model = Room
-        fields = "__all__"
+        exclude = ["amenities"]
 
     def get_rating(self, room):
         print(self.context)
@@ -43,6 +46,13 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(
+            user=request.user,
+            rooms__pk=room.pk,
+        ).exists()
 
 
 class RoomListSerializer(serializers.ModelSerializer):
