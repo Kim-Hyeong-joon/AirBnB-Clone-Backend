85ff0e46cfb5238262146fe152d789faf3aff82d
Hyeong-Joon <gudwns1020@naver.com>
2023-03-10 10:12:42 +0900

    #12.3 Log In and Log Out

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
     path("change-password", views.ChangePassword.as_view()),
+    path("log-in", views.LogIn.as_view()),
+    path("log-out", views.LogOut.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
     path("@<str:username>/reviews", views.PublicUserReviews.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -97,3 +98,30 @@ class PublicUserReviews(APIView):
         reviews = user.reviews.all()[start:end]
         serializer = ReviewSerializer(reviews, many=True)
         return Response(serializer.data)
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome!"})
+        else:
+            return Response({"error": "wrong password"})
+
+
+class LogOut(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "bye!"})
