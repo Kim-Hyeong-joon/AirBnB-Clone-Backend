d94e35a5691f46330e887e9f99e3dc1fde0d587e
Hyeong-Joon <gudwns1020@naver.com>
2023-03-21 12:25:01 +0900

    #20.7 Access Token

--- users/views.py
+++ users/views.py
@@ -153,18 +153,44 @@ class JWTLogIn(APIView):
 
 class GithubLogIn(APIView):
     def post(self, request):
-        code = request.data.get("code")
-        client_id = "54c145a4cd5b954d7bc7"
-        access_token = requests.post(
-            f"https://github.com/login/oauth/access_token?code={code}&client_id={client_id}&client_secret={settings.GH_SECRET}",
-            headers={"Accept": "application/json"},
-        )
-        access_token = access_token.json().get("access_token")
-        user_data = requests.get(
-            "https://api.github.com/user",
-            headers={
-                "Authorization": f"Bearer {access_token}",
-                "Accept": "application/json",
-            },
-        )
-        user_data = user_data.json()
+        try:
+            code = request.data.get("code")
+            client_id = "54c145a4cd5b954d7bc7"
+            access_token = requests.post(
+                f"https://github.com/login/oauth/access_token?code={code}&client_id={client_id}&client_secret={settings.GH_SECRET}",
+                headers={"Accept": "application/json"},
+            )
+            access_token = access_token.json().get("access_token")
+            user_data = requests.get(
+                "https://api.github.com/user",
+                headers={
+                    "Authorization": f"Bearer {access_token}",
+                    "Accept": "application/json",
+                },
+            )
+            user_data = user_data.json()
+            user_emails = requests.get(
+                "https://api.github.com/user/emails",
+                headers={
+                    "Authorization": f"Bearer {access_token}",
+                    "Accept": "application/json",
+                },
+            )
+            user_emails = user_emails.json()
+            try:
+                user = User.objects.get(email=user_emails[0]["email"])
+                login(request, user)
+                return Response(status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                user = User.objects.create(
+                    username=user_data.get("login"),
+                    email=user_emails[0]["email"],
+                    name=user_data.get("name"),
+                    avatar=user_data.get("avatar_url"),
+                )
+                user.set_unusable_password()
+                user.save()
+                login(request, user)
+                return Response(status=status.HTTP_200_OK)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
